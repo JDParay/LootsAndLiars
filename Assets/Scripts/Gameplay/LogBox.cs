4bd93f55a15809ab7e324e1a0b80619630673f87
Jeko Paray <jdparay@mcm.edu.ph>
2026-08-26 08:01:07 +0800

    Scrolling Log finally fixed

--- Assets/Scripts/Gameplay/LogBox.cs
+++ Assets/Scripts/Gameplay/LogBox.cs
@@ -6,11 +6,21 @@ public class LogBox : MonoBehaviour
 {
     public ScrollRect scrollRect;
     public TMP_Text logText;
+    public RectTransform contentRect;
+
+    [SerializeField] private float autoScrollThreshold = 0.05f;
 
     public void Log(string message)
     {
+        bool isAtBottom = scrollRect.verticalNormalizedPosition <= autoScrollThreshold;
+
         logText.text += (logText.text.Length > 0 ? "\n" : "") + message;
-        Canvas.ForceUpdateCanvases(); 
-        scrollRect.verticalNormalizedPosition = 0f; 
+        logText.ForceMeshUpdate();
+        LayoutRebuilder.ForceRebuildLayoutImmediate(contentRect);
+
+        if (isAtBottom)
+        {
+            scrollRect.verticalNormalizedPosition = 0f;
+        }
     }
 }
