43e35accc0d29e1d961e7b9cc110fe9fdfe4bcd5
Jeko Paray <jdparay@mcm.edu.ph>
2026-08-27 20:48:52 +0800

    display changes

--- Assets/Scripts/Gameplay/StatBox.cs
+++ Assets/Scripts/Gameplay/StatBox.cs
@@ -4,7 +4,6 @@ using TMPro;
 
 public class StatBox : MonoBehaviour
 {
-    public Image portraitImage;
     public TMP_Text strLabel;
     public TMP_Text hstLabel;
     public TMP_Text wisLabel;
@@ -12,9 +11,6 @@ public class StatBox : MonoBehaviour
 
     public void SetCharacter(CharacterData data)
     {
-        portraitImage.sprite = data.gameplayPortrait;
-        portraitImage.enabled = data.gameplayPortrait != null;
-
         strLabel.text = $"STR:\t{data.runtimeStats.strength}";
         hstLabel.text = $"HST:\t{data.runtimeStats.haste}";
         wisLabel.text = $"WIS:\t{data.runtimeStats.wisdom}";
